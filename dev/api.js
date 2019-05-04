128f29670f2996e8bf652faf3c2187de5381bbc0
Miyuki Abe <miyuki0809@gmail.com>
2019-05-04 10:48:33 -0400

    feat: compelted get all blockchain route

--- dev/api.js
+++ dev/api.js
@@ -1,6 +1,29 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const Blockchain = require('./blockchain')
+const bitcoin = new Blockchain()
 
-app.get('/', (res, req) =>{
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+//this is the page we can see existing blockchain
+app.get('/blockchain', (req, res) => {
+  res.send(bitcoin)
+})
+
+//this is the endpoints where we can post transaction
+app.post('/transaction', (req, res) => {
+  console.log(req.body)
+  res.send(`the amount of the transaction is ${req.body.amount}`)
+})
+
+//mine endpoint will mind a new Block for us or create a new block
+app.get('/mine', (req, res) => {
   
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}...`)
 })
